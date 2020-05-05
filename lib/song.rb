68097216de9151564a10694b8848abfbfff6bfad
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-05 16:28:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,10 @@ class Song
    def self.genre_count
      genre_count = {}
      @genres.each do |genre|
-       
+       if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
     end
     genre_count
    end
