001af30aeace5477b04a955af06a02cc845a737e
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-05 16:15:37 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+class Song 
+   
+   attr_accessor :name, :artist, :genre 
+   
+   @@count = 0 
+   
+   def initialize(name, artist, genre)
+     @name = name
+     @artist = artist
+     @genre = genre
+     @@count += 1 
+     @@genres << genre
+     @@artists << artist
+   end
+   
+   def self.count
+     @@count
+   end
+   
+   def self.genres 
+     @@genres = []
+     
+   end
+   
+   def self.artists 
+     @@artists = []
+     artist.uniq
+   end
+   
+   def self.artist_count
+    # returns histogram for artists
+   end
+   
+   def self.genre_count
+     genre_count = {}
+   end
+   
+   
+end
+
