43c1c5a556d31acd19da33fd6762a17460b79353
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-05 16:19:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,7 @@ class Song
    @@count = 0 
    @@artists = []
    
+   
    def initialize(name, artist, genre)
      @name = name
      @artist = artist
@@ -21,7 +22,7 @@ class Song
    end
    
    def self.genres 
-     @@genres = []
+     
      
    end
    
