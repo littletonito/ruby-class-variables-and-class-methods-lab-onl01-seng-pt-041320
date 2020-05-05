7c6ec238d833439858f4be4790180dbae29940f5
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-05 16:30:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,8 +44,14 @@ class Song
    
   def self.artist_count
      artist_count = {}
-     @artists.each do 
+     @artists.each do |artist|
+       if artist_count[artist]
+         artist_count[artist] += 1 
+       else
+         artist_count[artist] = 1 
+       end
       end
+      artist_count
   end
    
 end
